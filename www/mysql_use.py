180c8a6f007fd8ab01e40948d82bfb3052688213
arvon <yafeng2011@126.com>
2018-04-21 18:31:19 +0800

    Orm TS

--- www/mysql_use.py
+++ www/mysql_use.py
@@ -1,48 +1,14 @@
 
 from transwarp import db
+import asyncio
 
 db.create_engine(
     user='user1',
-    password='a45b3643364f7f',
-    database='web-app',
-    host='',
+    password='a4s7f',
+    database='web_app',
+    host='19afsd5.89',
     port='3306'
 )
 
-users = db.select('select * from user')
 
-n = db.update('insert into user(id, name) values(?, ?)', 4, 'Jack')
-
-with db.connection():
-    db.select('...')
-    db.update('...')
-
-with db.transaction():
-    db.select('...')
-    db.update('...')
-
-
-class _Engince(object):
-    def __init__(self, connect):
-        self._connect = connect
-    def connect(self):
-        return self.connect()
-
-engince = None
-
-class _DbCtx(thteading.local):
-    def __init__(self):
-        self.connection = None
-        self.transactions = 0
-    def is_init(self):
-        return not self.connection is None
-    def init(self):
-        self.connection = LasyConnection()
-        self.transactions = 0
-    def cleanup(self):
-        self.connection.cleanup()
-        self.connection = 0
-    def cursor(self):
-        return self.connection.cursor()
-
-_db_ctx = _DbCtx()
+@asyncio.coroutine
--- www/orm.py
+++ www/orm.py
@@ -2,16 +2,18 @@ import asyncio
 import logging
 import aiomysql
 
-@asyncio.coroutine
+def log(sql, args=()):
+    logging.info('SQL: %s' % sql)
+
 def create_pool(loop, **kw):
     logging.info('Create database connection pool...')
     global __pool
     __pool = yield from aiomysql.create_pool(
-        host=kw.get('host', 'localhost'),
+        host=kw.get('host', '192.144.135.189'),
         port=kw.get('port', 3306),
-        user=kw['user'],
-        password=kw['password'],
-        db=kw['db'],
+        user=kw['user1'],
+        password=kw['wd'],
+        db=kw['web_app'],
         charset=kw.get('charset', 'utf8'),
         autocommit=kw.get('autocommit', True),
         maxsize=kw.get('maxsize', 10),
@@ -19,7 +21,7 @@ def create_pool(loop, **kw):
         loop=loop
     )
 
-@asyncio.coroutine
+
 def select(sql, args, size=None):
     log(sql, args)
     global __pool
@@ -34,15 +36,68 @@ def select(sql, args, size=None):
         logging.info('Rows returned: %s' % len(rs))
         return rs
 
-@asyncio.coroutine
-def execute(sql, args):
+
+def execute(sql, args, autocommit=True):
     log(sql)
-    with (yield from __pool) as conn:
+    async with __pool.get() as conn:
+        if not autocommit:
+            await conn.begin()
         try:
-            cur = yield from conn.cursor()
-            yield from cur.execute(sql.replace('?', '%s'), % args)
-            affected = cur.rowcount
-            yield from cur.close()
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replace('?', '%s'), args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
         except BaseException as e:
+            if not autocommit:
+                await conn.rollback()
             raise
         return affected
+
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return  ', '.join(L)
+
+class Field(object):
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+class StringField(Field)
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+class BooleanField(Field):
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+class IntegerField(Field):
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+class FloatField(Field):
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+class TextField(Field):
+    def __init__(self, name=None,, default=None):
+        super().__init__(name, 'text', False, default)
+
+
+from orm import Model, StringField, IntegerField
+
+class User(Model):
+    __table__ = 'users'
+    id = IntegerField(primary_key=True)
+    name = StringField()
+
+user = User(id=007, name='Arvon')
+user.insert()
+users = User.findAll()
